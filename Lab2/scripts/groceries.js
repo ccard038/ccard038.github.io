f3a82558496bd70cbab9d8910b098eadef4649db
ccard038 <ccard038@uottawa.ca>
2021-05-30 13:49:31 -0400

    a test

--- Lab2/scripts/groceries.js
+++ Lab2/scripts/groceries.js
@@ -86,22 +86,58 @@ var products = [
 
 // given restrictions provided, make a reduced list of products
 // prices should be included in this list, as well as a sort based on price
+// There is for sure a better way of doing this than copy and pasting the same code for different condtions, however I'm doing this quick and dirty.
 
 function restrictListProducts(prods, restriction) {
   let product_names = [];
+
   for (let i = 0; i < prods.length; i += 1) {
     if (restriction == "Vegetarian" && prods[i].vegetarian == true) {
       product_names.push(prods[i].name);
     } else if (restriction == "GlutenFree" && prods[i].glutenFree == true) {
       product_names.push(prods[i].name);
-    } else if (restriction == "lactoseFree" && prods[i].lactoseFree == true) {
+    } else if (restriction == "LactoseFree" && prods[i].lactoseFree == true) {
       product_names.push(prods[i].name);
-    } else if (restriction == "nutFree" && prods[i].nutFree == true) {
+    } else if (restriction == "NutFree" && prods[i].nutFree == true) {
       product_names.push(prods[i].name);
     } else if (restriction == "All") {
       product_names.push(prods[i].name);
     }
   }
+  if (restriction.length === 4) {
+    document.getElementById("selected").innerHTML =
+      "Selected " +
+      restriction[0] +
+      " " +
+      restriction[1] +
+      " " +
+      restriction[2] +
+      " " +
+      restriction[3] +
+      "  items:";
+  }
+  if (restriction.length === 3) {
+    document.getElementById("selected").innerHTML =
+      "Selected " +
+      restriction[0] +
+      " " +
+      restriction[1] +
+      " " +
+      restriction[2] +
+      "  items:";
+  }
+  if (restriction.length === 2) {
+    document.getElementById("selected").innerHTML =
+      "Selected " + restriction[0] + " " + restriction[1] + "  items:";
+  }
+  if (restriction.length === 1) {
+    document.getElementById("selected").innerHTML =
+      "Selected " + restriction[0];
+  }
+  if (restriction.length === 0) {
+    document.getElementById("selected").innerHTML = "Please make a selection.";
+  }
+
   return product_names;
 }
 
